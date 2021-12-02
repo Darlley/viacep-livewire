a2d2c81f0190f4785d884dba9bc5f2a5fef91784
Darlley <systembrtt@gmail.com>
2021-12-02 15:20:43 -0400

    test api

--- tests/Feature/ViacepServiceTest.php
+++ tests/Feature/ViacepServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ViacepServiceTest extends TestCase
@@ -13,10 +14,15 @@ class ViacepServiceTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_if_viacep_api_is_woring()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $cep = '79036030';
+        $response = Http::get("viacep.com.br/ws/{$cep}/json/");
+        $this->assertSame(200, $response->status());
+    }
+    public function teste_if_response_is_an_array(){
+        $cep = '79036030';
+        $response = Http::get("viacep.com.br/ws/{$cep}/json/");
+        $this->assertTrue(Arr::accessible($response->json()));
     }
 }
